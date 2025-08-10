13f2573a300aafae5606fdc67d9df28a845c96b6
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-08-09 19:55:08 -0700

    separate loading content into a js module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css";
+import { addTagline } from "./load-content"
 
-console.log("Restaurant Page");
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+    addTagline();
+}
---
+++ src/load-content.js
@@ -0,0 +1,13 @@
+let content = document.getElementById("content");
+export function addTagline () {
+    let tagline = document.createElement("div");
+    let paragraph = document.createElement("p");
+    paragraph.textContent = "A Haven for the hungry heart and a harbor for the weary traveler"
+    tagline.class = "tagline";
+    tagline.appendChild(paragraph);
+    content.appendChild(tagline);
+}
+
+export function addHours () {
+    let content = document.getElementById("")
+}
